32af5363baefa4b71c1b05dd5755fa3b8a97c2a8
Renjith-nemesis <renjith.s@hashedin.com>
2019-07-22 19:14:29 +0530

    Working RoboApp

---
+++ src/RobApp.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RoboApp: React.FC = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Robo App
+          </p>
+        </header>
+      </div>
+    );
+  };
+  
+  export default RoboApp;
--- src/Robot.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-
-// class Toy extends Component{
-//     render(){
-//         return (
-//             <span></span>
-//         );
-//     }
-// }
